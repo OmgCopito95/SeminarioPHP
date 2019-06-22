172a757dc2a5f7b607a51b14a61ddd783360f5c7
Julieta <july1995bolt@gmail.com>
2019-06-22 15:30:06 -0300

    validador de publicarMensaje

---
+++ publicarMensaje.php
@@ -0,0 +1,25 @@
+<?php
+	
+	session_start();
+
+	if ($_SESSION["logueado"] == true){
+
+		include ("baseDeDatos.php");
+		include ("BD.php");
+
+		if($_SERVER["REQUEST_METHOD"]=="POST"){ // si se completo el formulario
+
+			$mensaje = $_POST["mensaje"]; // me guardo el mensaje que escribio
+			if (strlen($mensaje) <= 140){ // verifico que el texto ingresado no sea mayor a 140 caracteres
+				//falta la imagen
+				$bd = new BaseDeDatos($conn);
+				$bd->publicarMensaje($mensaje,1); // la guardo en la base de datos
+			}else{
+				$_SESSION["errores"]="Mensaje mayor a 140 caracteres.";
+			}
+
+    		header('Location: '."principal.php");
+
+		} 
+	}
+?>
