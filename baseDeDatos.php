1e09da9820c4651903597b5fd66a04be628859e1
Julieta <july1995bolt@gmail.com>
2019-06-21 23:46:47 -0300

    Agrega msqli_close() a cada consulta

--- baseDeDatos.php
+++ baseDeDatos.php
@@ -13,24 +13,31 @@ class BaseDeDatos {
     	$query = "SELECT * FROM usuarios WHERE nombreusuario='".$user."'";
 		$result = mysqli_query($this->link,$query) or die(mysqli_error($this->link));
 		//recupera una fila de resultado como un array
-    	return mysqli_fetch_array($result);
+		$resultado = mysqli_fetch_array($result);
+		mysqli_close($link);
+    	return $resultado;
     }
 
     function newUser($nombre, $apellido,$email,$user,$pass,$imagen,$tipo_imagen){
         $query = "INSERT INTO usuarios(apellido,nombre,email,nombreusuario,contrasenia,foto_contenido,foto_tipo) VALUES ('$apellido','$nombre','$email','$user','$pass','$imagen','tipo_imagen')";
         mysqli_query($this->link,$query) or die(mysqli_error($this->link));
+        mysqli_close($link);
 	}
 	
 	function checkPassword($password, $user){
 		$query = "SELECT contrasenia FROM usuarios WHERE nombreusuario='".$user."'";
 		$result = mysqli_query($this->link,$query) or die(mysqli_error($this->link));
 		if ($password == mysqli_fetch_array($result)[0]){
+			mysqli_close($link);
 			return true;
 		} else {
+			mysqli_close($link);
 			return false;
 		}
 	}
 
+
+	
 }
 
 ?>
