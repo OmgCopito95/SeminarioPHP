4d6266a946771043ac7a467bfe05992553999fc6
matiglesias <matidxg@gmail.com>
2019-06-22 02:11:52 -0300

    Login/reg no debe estar logueado; User a session.

--- validadorLogueo.php
+++ validadorLogueo.php
@@ -1,32 +1,42 @@
 <?php
 session_start();
-include ("claseValidador.php");
-include ("baseDeDatos.php");
-include ("BD.php");
+if ($_SESSION["logueado"] = false){
+  include ("claseValidador.php");
+  include ("baseDeDatos.php");
+  include ("BD.php");
 
-if ($_SERVER["REQUEST_METHOD"]=="POST"){ // si se completo el formulario
-  $validador = new Validador(); // instancio objeto validador
+  if ($_SERVER["REQUEST_METHOD"]=="POST"){ // si se completo el formulario
+    $validador = new Validador(); // instancio objeto validador
 
-  // verifico que el nombre de usuario cumpla
-  if ($validador->validarNombreUsuario($_POST["username"])){
-    $bd = new BaseDeDatos($conn);
-    $existe = $bd->getUser($_POST["username"]);
+    // verifico que el nombre de usuario cumpla
+    if ($validador->validarNombreUsuario($_POST["username"])){
+      $bd = new BaseDeDatos($conn);
+      $existe = $bd->getUser($_POST["username"]);
 
-    if ($existe[0]){
-        // verifico que la contrasenia cumpla
-        $pass = $_POST["password"];
-        if ($validador->validarContrasenias($pass,$pass)){ // se lo pasa dos veces para reutilizar metodo
-        echo($bd->checkPassword($_POST["password"], $_POST["username"]));
-        //echo $bd; 
-            if ($bd->checkPassword($_POST["password"], $_POST["username"])) {
+      if ($existe[0]){
+          // verifico que la contrasenia cumpla
+          $pass = $_POST["password"];
+          if ($validador->validarContrasenias($pass,$pass)){ // se lo pasa dos veces para reutilizar metodo
+            //echo($bd->checkPassword($pass, $_POST["username"]));
+            //echo $bd; 
+            $user = $_POST["username"];
+            if ($bd->checkPassword($pass, $user)) {
                 $_SESSION["logueado"] = true;
+                $_SESSION["usuario"] = $user;
                 header('Location: '."principal.php");
                 die();
             }
-        }
+          }
+      }
     }
+    // por seguridad no debo decirle que es lo que esta mal
+    $_SESSION["errores"] = "Usuario o contraseña incorrectos.";
+    header('Location: '."index.php");
+    die();
   }
-  // no informo nada xq por seguridad no debo decirle que es lo que esta mal
+}
+else {
+  $_SESSION["errores"] = "Sesion ya iniciada.";
   header('Location: '."index.php");
   die();
 }
--- validadorRegistro.php
+++ validadorRegistro.php
@@ -1,71 +1,74 @@
 <?php
 session_start();
+if ($_SESSION["logueado"] = false){
+  include ("claseValidador.php");
+  include ("baseDeDatos.php");
+  include ("BD.php");
+  include ("subirImagenes.php");
 
-include ("claseValidador.php");
-include ("baseDeDatos.php");
-include ("BD.php");
-include ("subirImagenes.php");
+  $error = array(); // creo un array que me guarde los mensajes de error
 
-$error = array(); // creo un array que me guarde los mensajes de error
+  if($_SERVER["REQUEST_METHOD"]=="POST"){ // si se completo el formulario
+    $validador = new Validador(); // instancio objeto validador
 
-if($_SERVER["REQUEST_METHOD"]=="POST"){ // si se completo el formulario
-  $validador = new Validador(); // instancio objeto validador
+    // verifico que el nombre cumpla
+    if (!$validador->validarNombreApellido($_POST["nombre"])){ 
+      $error[]="Nombre incorrecto."; // si no cumple: guardo el error en el array
+    }
+    
+    // verifico que el apellido cumpla
+    if (!$validador->validarNombreApellido($_POST["apellido"])){ 
+      $error[]="Apellido incorrecto.";
+    }
+    
+    // verifico que el email cumpla
+    if (!$validador->validarEmail($_POST["email"])){ 
+      $error[]="Email incorrecto.";
+    }
 
-  // verifico que el nombre cumpla
-  if (!$validador->validarNombreApellido($_POST["nombre"])){ 
-    $error[]="Nombre incorrecto."; // si no cumple: guardo el error en el array
-  }
-  
-  // verifico que el apellido cumpla
-  if (!$validador->validarNombreApellido($_POST["apellido"])){ 
-    $error[]="Apellido incorrecto.";
-  }
-  
-  // verifico que el email cumpla
-  if (!$validador->validarEmail($_POST["email"])){ 
-    $error[]="Email incorrecto.";
-  }
+    // verifico que el nombre de usuario cumpla
+    if (!$validador->validarNombreUsuario($_POST["user"])){ 
+      $error[]="Nombre de usuario incorrecto.";
+    } else {
+      //verifico que el usuario no exista en la bd
+      $bd = new BaseDeDatos($conn);
+      $existe = $bd->getUser($_POST["user"]);
+      //echo $existe[0];
+      if($existe[0]){ //si devuelve distinto de 0 o NULL significa que el usuario existe
+        $error[]="El nombre de usuario ya esta en uso";
+      }
+    }
 
-  // verifico que el nombre de usuario cumpla
-  if (!$validador->validarNombreUsuario($_POST["user"])){ 
-    $error[]="Nombre de usuario incorrecto.";
-  } else {
-    //verifico que el usuario no exista en la bd
-    $bd = new BaseDeDatos($conn);
-    $existe = $bd->getUser($_POST["user"]);
-    //echo $existe[0];
-    if($existe[0]){ //si devuelve distinto de 0 o NULL significa que el usuario existe
-      $error[]="El nombre de usuario ya esta en uso";
+    // verifico que las contrasenias cumplan
+    if (!$validador->validarContrasenias($_POST["pass1"],$_POST["pass2"])){ 
+      $error[]="Contraseñas incorrectas.";
+    }
+    
+    // si el array de errores esta vacio, entonces direcciono a la pagina principal
+    if((sizeof($error)==0) &&($imagenSubida == 1)){ // si el array de los errores no tiene elementos
+      //hay que crear el usuario e ir a la pagina principal
+
+      $bd = new BaseDeDatos($conn);
+
+      //echo $tipo_imagen;
+      //falta guardar la imagen del usuario.
+
+      $bd->newUser($_POST["nombre"],$_POST["apellido"],$_POST["email"],$_POST["user"],$_POST["pass1"],$archivo,$tipo_imagen);
+      
+      $_SESSION["logueado"] = true;
+      $_SESSION["usuario"] = $user;
+      header('Location: '."principal.php");
+      die();
+    } 
+    else {
+      $_SESSION["errores"] = $error; //guarda los errores en la sesion del usuario para poder usar la variable en la vista
+      header('Location: '."index.php");
+      die();
     }
   }
-
-  // verifico que las contrasenias cumplan
-  if (!$validador->validarContrasenias($_POST["pass1"],$_POST["pass2"])){ 
-    $error[]="Contraseñas incorrectas.";
-  }
-  
-  // si el array de errores esta vacio, entonces direcciono a la pagina principal
-  if((sizeof($error)==0) &&($imagenSubida == 1)){ // si el array de los errores no tiene elementos
-    //hay que crear el usuario e ir a la pagina principal
-
-    $bd = new BaseDeDatos($conn);
-
-    //echo $tipo_imagen;
-    //falta guardar la imagen del usuario.
-
-    $bd->newUser($_POST["nombre"],$_POST["apellido"],$_POST["email"],$_POST["user"],$_POST["pass1"],$archivo,$tipo_imagen);
-    
-    $_SESSION["logueado"] = true;
-
-    header('Location: '."principal.php");
-    die();
-  } 
-  else {
-    $_SESSION["errores"] = $error; //guarda los errores en la sesion del usuario para poder usar la variable en la vista
-    header('Location: '."index.php");
-    die();
-  }
-
 }
-
+else {
+  $_SESSION["errores"] = "No se puede registrar estando logueado.";
+  header('Location: '."index.php");
+  die();}
 ?>
