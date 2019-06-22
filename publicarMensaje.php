9ecc0043ddc05f9152ecb92b2aabef158239787b
Julieta <july1995bolt@gmail.com>
2019-06-22 17:50:23 -0300

    Publicar mensaje con imagen

--- publicarMensaje.php
+++ publicarMensaje.php
@@ -1,25 +1,24 @@
 <?php
-	
-	session_start();
 
-	if ($_SESSION["logueado"] == true){
+	session_start();
+	if (!empty($_SESSION["logueado"])){
 
 		include ("baseDeDatos.php");
 		include ("BD.php");
+		include ("subirImagenes.php");
 
 		if($_SERVER["REQUEST_METHOD"]=="POST"){ // si se completo el formulario
-
 			$mensaje = $_POST["mensaje"]; // me guardo el mensaje que escribio
 			if (strlen($mensaje) <= 140){ // verifico que el texto ingresado no sea mayor a 140 caracteres
 				//falta la imagen
+				
 				$bd = new BaseDeDatos($conn);
-				$bd->publicarMensaje($mensaje,$_SESSION["id"],'',''); // la guardo en la base de datos
+				$bd->publicarMensaje($mensaje,$_SESSION["id"],$contents,$tipo_imagen); // la guardo en la base de datos
+
 			}else{
 				$_SESSION["errores"]=array("Mensaje mayor a 140 caracteres.");
 			}
-
     		header('Location: '."principal.php");
-
 		} 
 	}
 ?>
