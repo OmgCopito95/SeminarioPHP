83e9c08441594d092db709ea3fa667128fb3807f
Julieta <july1995bolt@gmail.com>
2019-06-22 16:35:04 -0300

    Correccion query publicarMensaje / newUser

--- baseDeDatos.php
+++ baseDeDatos.php
@@ -19,9 +19,8 @@ class BaseDeDatos {
     }
 
     function newUser($nombre, $apellido,$email,$user,$pass,$imagen,$tipo_imagen){
-        $query = "INSERT INTO usuarios(apellido,nombre,email,nombreusuario,contrasenia,foto_contenido,foto_tipo) VALUES ('$apellido','$nombre','$email','$user','$pass','$imagen','tipo_imagen')";
+        $query = "INSERT INTO usuarios(apellido,nombre,email,nombreusuario,contrasenia,foto_contenido,foto_tipo) VALUES ('$apellido','$nombre','$email','$user','$pass','$imagen','$tipo_imagen')";
         mysqli_query($this->link,$query) or die(mysqli_error($this->link));
-        mysqli_close($link);
 	}
 
 	function editUser($datos, $user){
@@ -36,26 +35,23 @@ class BaseDeDatos {
 		}
 		$query = $query." WHERE nombreusuario = '$user'";
 		mysqli_query($this->link,$query) or die(mysqli_error($this->link));
-        // mysqli_close($link);
 	}
 	
 	function checkPassword($password, $user){
 		$query = "SELECT contrasenia FROM usuarios WHERE nombreusuario='".$user."'";
 		$result = mysqli_query($this->link,$query) or die(mysqli_error($this->link));
 		if ($password == mysqli_fetch_array($result)[0]){
-			// mysqli_close($link);
 			return true;
 		} else {
-			mysqli_close($link);
 			return false;
 		}
 	}
 
 
-	function publicarMensaje($mensaje, $userID){
+	function publicarMensaje($mensaje, $userID, $imagen, $tipo_imagen){
         //$mysqltime = date("Y-m-d H:i:s");
         //echo $mysqltime;
-        $query = "INSERT INTO mensaje (texto,imagen_contenido,imagen_tipo,usuarios_id,fechayhora)VALUES('$mensaje', '','',$userID,'')";
+        $query = "INSERT INTO mensaje (texto,imagen_contenido,imagen_tipo,usuarios_id,fechayhora)VALUES('$mensaje', '$imagen','$tipo_imagen',$userID,'')";
         mysqli_query($this->link,$query) or die(mysqli_error($this->link));
     }
 }
