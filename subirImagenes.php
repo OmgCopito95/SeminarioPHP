a874979ed8c11fed8a73e8137d3da2859660deaa
Julieta <july1995bolt@gmail.com>
2019-06-23 18:36:59 -0300

    Errores en session en subirImagenes

--- subirImagenes.php
+++ subirImagenes.php
@@ -4,14 +4,14 @@ $archivo = $directorio . basename($_FILES["pic"]["name"]); //nombre del archivo
 $ok = 1;
 $tipo_imagen = strtolower(pathinfo($archivo,PATHINFO_EXTENSION)); //se guarda la extension
 
+$error = array(); // creo un array que me guarde los mensajes de error
 // Verifica si realmente es una imagen la que se esta subiendo
 if(isset($_POST["pic"])) {
     $check = getimagesize($_FILES["archivo"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
         $ok = 1;
     } else {
-        echo "File is not an image.";
+        $error[]= "El archivo no es una imagen";
         $ok = 0;
     }
 }
@@ -19,13 +19,14 @@ if(isset($_POST["pic"])) {
 // Verifica que se suban formatos correctos
 if($tipo_imagen != "jpg" && $tipo_imagen != "png" && $tipo_imagen != "jpeg"
 && $tipo_imagen != "gif" ) {
-    echo "Solo archivos JPG, JPEG, PNG & GIF están permitidos.";
+    //$error[]= "Solo archivos JPG, JPEG, PNG & GIF están permitidos.";
     $ok = 0;
 }
 
 // si la variable ok=0, hubo un error y no se puede cargar la imagen
-if ($ok == 0) {
-    echo "Su imagen no puede subirse.";
+if (($ok == 0)){
+    $error[]="No se publico ninguna imagen.";
+    $_SESSION["errores"] = $error;
 } else {
     if (move_uploaded_file($_FILES["pic"]["tmp_name"], $archivo)) { // guardo archivo en el directorio
     	$imagenSubida = 1;
@@ -34,7 +35,8 @@ if ($ok == 0) {
         //echo "El arhivo ". basename( $_FILES["pic"]["name"]). "fue subido correctamente.";
         unlink($archivo);
     } else {
-        echo "Hubo un problema subiendo la imagen.";
+        $error[]="Hubo un problema subiendo la imagen.";
+        $_SESSION["errores"] = $error;
     }
 }
 
