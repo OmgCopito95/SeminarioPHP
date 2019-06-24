80fe726c8201d445e5ed8137ab3b5808d1349bc1
matiglesias <matidxg@gmail.com>
2019-06-23 21:22:19 -0300

    Creada vista y validador de editar contraseña

---
+++ editarContrasenia.php
@@ -0,0 +1,114 @@
+<?php
+  session_start();
+  if ($_SESSION["logueado"] == false) {
+    header('Location: '."index.php");
+    die();
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+  <head>
+    <title>The Wall - Editar perfil</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style/css/layout.css" type="text/css">
+    <link rel="stylesheet" href="style/css/profile.css" type="text/css">
+    <link rel="stylesheet" href="style/css/tables.css" type="text/css">
+    <link rel="stylesheet" href="style/css/pagination.css" type="text/css">
+    <!-- iconos -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+  </head>
+
+  <body>
+    <!-- seccion 1 -->
+    <div class="section row1">
+      <header id="header">
+        <div id="hgroup">
+          <img src="style/images/logo.png" />
+          <h1><a href="principal.php">The Wall</a></h1> <!-- titulo -->
+          <h2>Nueva red social - Conecta con tus amigos!</h2> <!-- subtitulo -->
+        </div>
+        <nav> <!-- menu con los links -->
+          <ul>
+            <li><a href="principal.php">Inicio</a></li>
+            <li>
+              <div class="dropdown">
+                <a>Perfil</a>
+                <div class="dropdown-content">
+                <a href="perfil.php">Mi Perfil</a>
+                <a href="editar-perfil.php">Editar Perfil</a>
+                <a href="index.php">Cerrar Sesión</a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <form action="validadorBusqueda.php" method="post"> <!-- buscar -->
+                <fieldset>
+                  <input type="text" name="buscar" value="">
+                  <input type="submit" id="b_submit" value="Buscar">
+                </fieldset>
+              </form>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div> <!-- fin seccion 1 -->
+
+    <!-- ERRORES -->
+    <div style="color: red;"> 
+      <?php
+        //recorro el array de errores para devolver si hay algun campo mal ingresado
+        if (!empty($_SESSION["errores"])){
+          $error = $_SESSION["errores"];
+          for ($i=0; $i < sizeof($error) ; $i++) { 
+            echo "Error: ".$error[$i]."<br>";
+          }
+          unset($_SESSION["errores"]);
+        }    
+         
+      ?> 
+    </div>
+
+    <!-- seccion 2 -->
+    <div class="section row2">
+      <div id="container" align="center">
+
+        <br>
+          <form id="regform" style="border: 0px;" method="POST" action="validadorEditarContrasenia.php" onsubmit="<!--return(validarForm())-->" enctype="multipart/form-data"<!-- para subir imagen -->
+            <h1 class="title-pen">Modificar contraseña</h1></h2>
+
+            <div class="formfield">
+              <label for="password1"><b>Contraseña actual</b></label>
+              <input type="password" name="passwordActual" id="passwordActual">
+            </div>
+
+            <div class="formfield">
+              <label for="password1"><b>Nueva contraseña</b></label>
+              <input type="password" name="password1" id="password1">
+            </div>
+
+            <div class="formfield">
+              <label for="password2"><b>Repita su contraseña nueva</b></label>
+              <input type="password" name="password2" id="password2">
+            </div>
+
+            <button name="savechanges" id="editbutton" type="submit">Guardar cambios</button>
+          </form>
+        <br>
+
+      </div>
+      <br>
+    </div> <!-- fin seccion 2 -->
+
+    <!-- seccion 3 - footer -->
+    <div class="section row3">
+      <footer id="footer">
+        <p class="fo_left">Hecho por: Iglesias Matias - Lanciotti Julieta</p>
+        <p class="fo_right">Seminario PHP 2019 </p>
+      </footer>
+    </div>
+
+  </body>
+</html>
---
+++ validadorEditarContrasenia.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+if ($_SESSION["logueado"] == true){
+  include ("claseValidador.php");
+  include ("baseDeDatos.php");
+  include ("BD.php");
+
+  $error = array(); // creo un array que me guarde los mensajes de error
+
+  if ($_SERVER["REQUEST_METHOD"]=="POST") { // si se completo el formulario
+    $validador = new Validador(); // instancio objeto validador
+    $cantIngresados = 0;
+
+    // verifico que la contraseña actual cumpla
+    if (!empty($_POST["passwordActual"])) {
+      $cantIngresados++;
+      if (!$validador->validarContrasenias($_POST["passwordActual"],$_POST["passwordActual"])){ 
+        $error[]="Contraseña invalida.";
+      } else {
+        $bd = new BaseDeDatos($conn);
+        if (!$bd->checkPassword($_POST["passwordActual"], $_SESSION["usuario"])) {
+          $error[]="Contraseña incorrecta.";
+        }
+      }
+    
+      // la contraseña nueva debe estar ingresada las dos veces
+      if (!empty($_POST["password1"]) && !empty($_POST["password2"])) {
+        $cantIngresados++;
+        if ($validador->validarNuevaContrasenia($_POST["passwordActual"], $_POST["password1"])) {
+          if (!$validador->validarContrasenias($_POST["password1"],$_POST["password2"])){ 
+            $error[]="Contraseñas incorrectas.";
+          }
+          else {
+            $nuevapass["contrasenia"] = $_POST["password1"];
+          }
+        }
+        else {
+          $error[] = "La contraseña nueva igual a la anterior.";
+        }
+      }
+      else {
+        $error[] = "Debe ingresar dos veces la nueva contraseña.";
+      }
+    }
+
+    if ($cantIngresados > 0) {
+      $_SESSION["errores"] = $error;
+      // si el array de errores esta vacio, entonces direcciono a la pagina principal
+      if (sizeof($error)==0) { // si el array de los errores no tiene elementos
+        //hay que crear el usuario e ir a la pagina principal
+        $bd = new BaseDeDatos($conn);
+        //falta guardar la imagen del usuario.
+  
+        $bd->editUser($nuevapass, $_SESSION["usuario"]);
+  
+        header('Location: '."editar-perfil.php");
+        die();
+      }
+      else {
+        // for ($i=0; $i < sizeof($error); $i++) { 
+        //   echo $error[0];
+        // }
+        header('Location: '."editarContrasenia.php");
+        die();
+      }
+    }
+    else {
+      $_SESSION["errores"] = array("No ha ingresado ningun dato para actualizar.");
+      header('Location: '."editarContrasenia.php");
+      die();
+    }
+  }
+  else {
+    // tendria que hacer algo raro para que no sea un POST, pero por si acaso..
+    $_SESSION["errores"] = array("No seas malo");
+    header('Location: '."editarContrasenia.php");
+    die();
+  }
+}
+else {
+  $_SESSION["errores"] = array("Sesion no iniciada.");
+  header('Location: '."index.php");
+  die();
+}
+?>
